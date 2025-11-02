457daee8c2f10cfc332e7da2aea56fa0a48e0e56
pr3! <149943933+the1anonlypr3@users.noreply.github.com>
2025-11-03 00:21:51 +1000

    delete random thing i used to try lists

--- programs/camping.py
+++
@@ -1,5 +0,0 @@
-supplies = ["tent", "sleeping bag", "water", "raspberry pi", "coffee", "knife", "ethernet cable", "flash drive", "beard oil", "marshmallows"]
-campsite = ["Crystal Lake", 404, True, 89.3]
-
-you = supplies[9]
-print(you)
